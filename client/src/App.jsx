5923dbe6de432d157e5acf405c2987a13229e5cd
felibunni <179690453+felibunni@users.noreply.github.com>
2025-09-12 22:40:55 +0200

    Screen reader/ARIA friendly route structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import './App.css'
+  import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+  import Header from './components/Header/Header';
+  import Footer from './components/Footer/Footer';
 
+  import Home from './pages/Home/Home';
+
+  //screen reader friendly structure with ARIA roles
+  function RootLayout() {
+    return (
+      <>
+        <header role="banner"> 
+          <Header /> 
+        </header>
+
+        <main id="page-content">
+          <Outlet />
+        </main>
+
+        <footer role="contentinfo">
+          <Footer />
+        </footer>
+      </>
+    );
+  }
+
+ function App() {
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+ }
 
-export default App
+ export default App;
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  
+
+  return (
+    <>
+      <h1>Felicia Bloom</h1>
+      <p>Välkommen till min portfölj!</p>
+    </>
+  )
+}
+
+export default Header;
