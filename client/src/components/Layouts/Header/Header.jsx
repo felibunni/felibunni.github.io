3693948aa71f86c9d9980571fcc9edea906a39fd
felibunni <179690453+felibunni@users.noreply.github.com>
2025-09-27 15:07:07 +0200

    Better component structure, centered header, root css changed, added md

---
+++ client/src/components/Layouts/Header/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+function Header() {
+  
+  return (
+    <>
+      <h1>Felicia Bloom</h1>
+      
+      <nav>
+        <ul>
+          <li>Start</li>
+          <li>Arbete</li>
+          <li>Om mig</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Header;
